c1cacac6257c4022a6dcaa998e8a42a4da0b4844
Hossein <hosein802000@gmail.com>
2026-07-19 14:50:24 +0330

    project done. need some work for deployment

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -39,11 +39,15 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # local
     "todos.apps.TodosConfig",
+    # 3rd Party
+    "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -122,3 +126,20 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# rest framework configs:
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSESS": [
+        "rest_framework.permissions.AllowAny",
+    ]
+}
+
+CORS_ALLOWED_ORIGINS = (
+    "hhtp://localhost:3000",  # default port for React
+    "http://localhost:8000",
+)
+
+CSRF_TRUSTED_ORIGINS = [
+    "localhost:3000"
+]  # to be able to connect to any front-end that uses port 3000
