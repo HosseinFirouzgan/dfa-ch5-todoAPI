ddc52bf0ac8ea913853c8300c947ee1717266d5b
Hossein <hosein802000@gmail.com>
2026-07-20 18:16:23 +0330

    fixed a typo in the rest_framework settings

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -142,7 +142,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # rest framework configs:
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSESS": [
+    "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
