aaa77dcbf0765b0afde5f0877f3d0a10e8f462d5
Hossein <hosein802000@gmail.com>
2026-08-03 17:50:25 +0330

    adding pass change using TDD method. Tests added.

--- users/tests.py
+++ users/tests.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 
 User = get_user_model()
 
@@ -195,3 +194,119 @@ class LogoutViewTests(APITestCase):
             format="json",
         )
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PasswordChangeViewTests(APITestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.old_password = "Underworldemon123"
+        cls.user = User.objects.create_user(
+            username="crowly",
+            email="crowlytheking@hell.com",
+            password=cls.old_password,
+        )
+        cls.new_password = "NewKing123"
+
+    def authenticate(self):
+        response = self.client.post(
+            reverse("login"),
+            {"username": self.user.username, "password": self.old_password},
+        )
+
+        access = response.data["access"]
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {access}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_authenticated_user_can_change_password(self):
+        self.authenticate()
+
+        response = self.client.post(
+            reverse("password_change"),
+            {
+                "old_password": self.old_password,
+                "new_password": self.new_password,
+                "new_password2": self.new_password,
+            },
+            format="json",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.check_password(self.new_password))  # new pass works
+        self.assertFalse(
+            self.user.check_password(self.old_password)
+        )  # old pass does'nt work
+
+    def test_wrong_old_password_get_rejected(self):
+        self.authenticate()
+
+        response = self.client.post(
+            reverse("password_change"),
+            {
+                "old_password": "wrong-pass",
+                "new_password": self.new_password,
+                "new_password2": self.new_password,
+            },
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("old_password", response.data)
+
+    def test_new_passwords_must_match(self):
+        self.authenticate()
+
+        response = self.client.post(
+            reverse("password_change"),
+            {
+                "old_password": self.old_password,
+                "new_password": self.new_password,
+                "new_password2": "something_different",
+            },
+            format="json",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("new_password2", response.data)
+
+    def test_weak_password_gets_rejected(self):
+        self.authenticate()
+
+        response = self.client.post(
+            reverse("password_change"),
+            {
+                "old_password": self.old_password,
+                "new_password": "123456",
+                "new_password2": "123456",
+            },
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("new_password", response.data)
+
+    def test_anonymous_user_cannot_change_password(self):
+
+        response = self.client.post(
+            reverse("password_change"),
+            {
+                "old_password": self.old_password,
+                "new_password": self.new_password,
+                "new_password2": self.new_password,
+            },
+            format="json",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_new_password_cannot_be_same_as_old_password(self):
+        self.authenticate()
+
+        response = self.client.post(
+            reverse("password_change"),
+            {
+                "old_password": self.old_password,
+                "new_password": self.old_password,
+                "new_password2": self.old_password,
+            },
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
